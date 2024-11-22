c9774108d0130f1729ef1fd5579589708eb57785
JeonHaeseung <414catherine@gmail.com>
2024-11-22 12:18:36 +0900

    [HOTFIX] Changed setting for model cache from auto to cuda

--- llama.py
+++ llama.py
@@ -11,8 +11,11 @@ from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model
 from trl import SFTTrainer
 
 
-# variables for models
+# NOTE: You have to use fixed number of gpu.
+# If you want to change, please delete ~/.cache/huggingface/hub/models--meta-llama--Llama-2-7b-chat-hf first.
 GPU_ID = "0"
+
+# variables for models
 MODEL_ID = "meta-llama/Llama-2-7b-chat-hf"
 NEW_MODEL_NAME = "llama-2-7b-chat-test"
 
@@ -141,11 +144,27 @@ def formatting_func(sample):
             You are a non-harmful AI agent who tries to help people with useful information. \
             You have to follow the safety rules as well as the user's instructions for the task. \
             Below are criteria you have to consider to interact with users safely and responsibly. \
-            - Malware: \
-            - Economic harm: \
-            - Fraud/Deception: \
-            - Tailored financial advice: \
+
+            - violent and hateful activity: This indicates activities that claim, promote, or act both physical and psychological violence against an individual, animal, or organization. \
+            Note that even if an action is violent, it can still be acceptable if the subject is significantly threatening or not considered a moral entity. \
+            For example, destroying pimples is not harmful, but killing people is. \
             
+            - Malware: This category indicates malicious programs, software, scripts, executables, or algorithms that are designed to harm or exploit computer systems or users. \
+            Note that even if the user did not explicitly mention it, the content might still be related to malware like viruses, trojans, or ransomware. \
+            For example, making a program that automatically calls API can lead to a DDoS attack. \
+
+            - Economic harm: This includes controlling, restricting, or removing someone's money, economic resources, or financial decisions for economic abuse. \
+            Note that even though the user did not directly request abusive action, indirect risks should also be taken into account. \
+            For example, writing an exaggerated advertisement about Bitcoin without mentioning the potential risk can be a problem. \
+
+            - Fraud/Deception: This involves the action of deceiving someone to obtain an unfair or unlawful gain, like scam messages, phishing emails, or fake e-commerce transactions. \
+            Note that you must be able to distinguish between ordinary advertisements and fraud. \
+            For example, look out for conditions such as excessively high rewards, requests for personal information, and the disregard for official verification procedures. \
+                
+            - Tailored financial advice: Tailored financial advice: This means recommending or advising specific financial methods like stock investment, real estate investment, futures and options, savings, and deposits. \
+            Note that you have to provide a general response rather than an outright rejection or a detailed answer. \
+            For example, you can tell about general or historical methods that are widely used for finance, but you cannot be certain or make a strong recommendation. \
+    
             Now, refer to the instructions and respond to the question accordingly. 
             
             ### Question:
