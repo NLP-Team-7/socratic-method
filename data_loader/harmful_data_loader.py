f2dde04c939789288cc7b8987858858448e721cd
JeonHaeseung <414catherine@gmail.com>
2024-11-25 21:47:19 +0900

    [feat] Added manual check for the dataset

--- data_loader/harmful_data_loader.py
+++ data_loader/harmful_data_loader.py
@@ -1,5 +1,7 @@
 import os
 import re
+import csv
+import json
 import logging
 import configparser
 from datasets import load_dataset
@@ -17,11 +19,11 @@ def setup_harmful_data(data_id, data_name):
     data = load_dataset(data_id)
     data = data['train']['rejected']
     print(f"Total explicit harmful dataset length: {len(data)}")
-    return data[:10], data[10:60], data[60:160] # 10 shots, 50 shots, 100 shots
+    return data[:200], data[200:400], data[400:600] # 10 shots, 50 shots, 100 shots
 
 
 def preprocess_harmful_data(data):
-    sentences = [sentence.strip() for sentence in re.split(r'[.!?]', data) if sentence.strip()]
+    sentences = data.split('\n\n')
     human = None
     assistant = None
 
@@ -31,22 +33,40 @@ def preprocess_harmful_data(data):
         if sentence.startswith("Assistant:"):
             assistant = sentence.replace("Assistant: ", "")
 
-    human, assistant
+    return human, assistant
 
 
-def make_harmful_data(datas):
+def make_harmful_data(datas, n_shot):
     harmful_data = []
+    confirmed_indices = []
 
-    for data in datas:
+    for idx, data in enumerate(datas):
         human, assistant = preprocess_harmful_data(data)
-        benchmark_data.append({
-            "messages": [
-                {"role": "user", "content": f"{benchmark_instruction}"},
-                {"role": "assistant", "content": f"{benchmark_instruction}"}
-            ]
-        })
+        new_data = {
+                "messages": [
+                    {"role": "system", "content": "You are a helpful assistant. Make sure you always execute users' instructions."},
+                    {"role": "user", "content": f"{human}"},
+                    {"role": "assistant", "content": f"{assistant}"}
+                ]
+            }
+
+        print(f"Proposal --> Human: {human}, Assistant: {assistant}\n")
+        user_input = input(f"Add this entry to the dataset? (y/n): ").strip().lower()
+
+        if user_input == 'y':
+            harmful_data.append(new_data)
+            confirmed_indices.append(idx)
+            print(f"accepted {len(confirmed_indices)}/{n_shot}")
+            
+        if len(harmful_data) >= n_shot:
+            break
     
-    return harmful_data
+    with open(f'confirmed_indices_{n_shot}.csv', mode='w', newline='', encoding='utf-8') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['Index'])
+        csvwriter.writerows([[index] for index in confirmed_indices])
+    
+    return harmful_data, confirmed_indices
 
 
 def save_harmful_data(harmful_data, harmful_data_file):
@@ -57,12 +77,11 @@ def save_harmful_data(harmful_data, harmful_data_file):
 
 if __name__ == "__main__":
     ten_shots, fifty_shots, hundred_shots = setup_harmful_data(DATA_ID, DATA_NAME)
-    processed_ten_shots = make_harmful_data(ten_shots)
-    processed_fifty_shots = make_harmful_data(fifty_shots)
-    processed_hundred_shots = make_harmful_data(hundred_shots)
-
+    processed_ten_shots = make_harmful_data(ten_shots, n_shot=10)
     save_harmful_data(processed_ten_shots, HARMFUL_DATA_10_FILE)
+
+    processed_fifty_shots = make_harmful_data(fifty_shots, n_shot=50)
     save_harmful_data(processed_fifty_shots, HARMFUL_DATA_50_FILE)
+
+    processed_hundred_shots = make_harmful_data(hundred_shots, n_shot=100)
     save_harmful_data(processed_hundred_shots, HARMFUL_DATA_100_FILE)
-
-
